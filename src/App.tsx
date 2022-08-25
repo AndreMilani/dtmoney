e1630299a43cda35562e9ea5b477c96abec58cc7
AndreMilani <luisandremilani@gmail.com>
2022-08-24 23:00:43 -0300

    primeiro commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import './App.css';
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <h1 className="title">Hello World</h1>
-      
-    </div>
+    <>
+    
+    <Header />
+
+    <GlobalStyle />
+
+    </>
   );
 }
 
